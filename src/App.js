55dcca7d1b7ef121362e5de62001ba747c2bfa4e
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-20 16:57:05 -0400

    add state, Search component, BASE_URL, APP_TOKEN_PARAMS, handleSearch and handleOnChange

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Search from './components/Search';
 import './App.css';
 
+const BASE_URL = 'https://data.cityofnewyork.us/resource/9w7m-hzhe.json?'
+const APP_TOKEN_PARAMS = '$$app_token='
+
 class App extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            searchQuery: '',
+            restaurantList: [],
+        }
+    }
+
+    handleSearch = ( e ) => {
+        e.preventDefault();
+    }
+    
+    handleOnChange = ( e ) => {
+        this.setState({ searchQuery: e.target.value });
+    }
+
     render() {
         return (
         <div className="App">
             <Header />
+            <Search handleOnSearchCB={ this.handleSearch } onChangeCB={ this.handleOnChange } />
         </div>
         );
     }
