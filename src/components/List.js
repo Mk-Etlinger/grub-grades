c016f848864353a764078f6d142a8acb0bd3c3e0
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-20 21:52:21 -0400

    update uniqueRestaurants to filter out non uniques, update displaRestaurants with key and violation data

--- src/components/List.js
+++ src/components/List.js
@@ -1,24 +1,30 @@
 import React from 'react';
 
 const List = ({ restaurants }) => {
-
-    const uniqueRestaurants = restaurants.filter(( rest, i ) => {
-        return i > 0 ? restaurants[i - 1].camis !== rest.camis : rest
+    const uniqueIDs = [];
+    const uniqueRestaurants = restaurants.filter( rest, i => {
+        let isUnique = false;
+        if ( !uniqueIDs.includes( rest.camis )) {
+            isUnique = true;
+            uniqueIDs.push( rest.camis );
+        }
+        return isUnique
     })
-    console.log(uniqueRestaurants)
 
     const displayRestaurants = uniqueRestaurants.map( rest => {
         // abstract out to table component
         return (
-            <div>
+            <div key={ rest.camis }>
                 <h1>{ rest.dba }</h1>
                 <h2>{ rest.grade }</h2>
                 <h4>{ rest.building + ' ' + rest.street }</h4>
                 <h4>{ rest.boro }</h4>
+                <h4>{ rest.violation_description }</h4>
+                <h4>{ rest.critical_flag }</h4>
             </div>
         )
     } )
-    console.log(displayRestaurants)
+    // console.log(displayRestaurants)
     return ( 
         <div>
             { displayRestaurants }
