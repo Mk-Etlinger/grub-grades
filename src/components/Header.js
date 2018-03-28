017243a88f815be1f5f9177470f2b56d782ef82c
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-28 10:44:00 -0400

    add title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({ title }) => {
     return ( 
         <header className="App-header">
-            <h1 className="App-title">Grub Grades</h1>
+            <h1 className="App-title">{ title }</h1>
         </header>
     )
 }
