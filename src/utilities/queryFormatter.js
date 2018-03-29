51988ee1760e300c61ee337bd567669295d8e825
Mike Etlinger <mk-etlinger@gmail.com>
2018-03-29 15:47:42 -0400

    remove upcaseString import, update variables for easier reading, add gradeNotNull const

--- src/utilities/queryFormatter.js
+++ src/utilities/queryFormatter.js
@@ -1,18 +1,21 @@
-import { upcaseString } from './upcaseString';
-
 const API_URL = process.env.REACT_APP_API_URL
 const API_TOKEN = `&$$app_token=${ process.env.REACT_APP_API_TOKEN }`
 
 export const queryFormatter = ( restaurantName ) => {
-    const upperCasedName = upcaseString( restaurantName ),
-        queries = whereParams( replaceQuotes( upperCasedName )) + orderByParams + API_TOKEN
+    const formattedQuery = replaceQuotes( restaurantName.toUpperCase() ),
+        queries = whereNameLikeParams( formattedQuery ) + 
+            gradeNotNull +
+            orderByParams +
+            API_TOKEN
 
     return encodeURI( API_URL + queries )
 }
 
-const whereParams = ( restaurantName ) => (
-    `$where=dba LIKE '%${ restaurantName }%' AND grade IS NOT NULL`
+const whereNameLikeParams = ( restaurantName ) => (
+    `$where=dba LIKE '%${ restaurantName }%' `
 )
+
+const gradeNotNull = 'AND grade IS NOT NULL'
 
 const orderByParams = '&$order=grade_date DESC'
 
